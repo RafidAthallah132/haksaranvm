2c60e1b9fa0f9371d029376f29bd760b440d30bb
bahariandikoo <bahariandikoo@gmail.com>
2022-04-25 08:26:41 +0700

    nambah route level (diko)

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('level', function () {
+    return view('/haksara/level');
+});
 
 Route::get('/', function () {
     return view('/haksara/halamanisilevel');
